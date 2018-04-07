ed939211308c2347ccee161445d8757bba724634
Andrew Mulvaney <AJMulv9878@gmail.com>
2018-04-06 23:43:23 -0400

    Second

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => (
   <nav className="navbar">
     <ul>
       <li className="brand">
-        <a href="/">Clicky Game</a>
+        <a href="/">GoT Card Game</a>
       </li>
       <li>Click an image to begin!</li>
       <li>Score: 0 | Top Score: 0</li>
